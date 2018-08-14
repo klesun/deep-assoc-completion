klesun/deep-assoc-completion

commit 1f8bd030ef74a59648461080e34bb3bba8211ef0

#41 Resolve array element type on demand

src/org/klesun/deep_assoc_completion/DeepType.java
@@ -25,8 +25,8 @@ public class DeepType extends Lang
     // just like array keys, but dynamic object properties
     public final Dict<Key> props = new Dict<>(L());
     // possible typeGetters of list element
-    public L<DeepType> anyKeyElTypes = new L<>();
-    public L<DeepType> listElTypes = new L<>();
+    public L<S<MultiType>> anyKeyElTypes = new L<>();
+    public L<S<MultiType>> listElTypes = new L<>();
     // applicable to closures and function names
     // (starting with self::) and [$obj, 'functionName'] tuples
     // slowly migrating returnTypes from constant values to a function
@@ -94,9 +94,9 @@ public class DeepType extends Lang
         return result;
     }
 
-    public L<DeepType> getElemTypes()
+    public L<DeepType> getListElemTypes()
     {
-        return listElTypes.cct(anyKeyElTypes);
+        return listElTypes.cct(anyKeyElTypes).fap(s -> s.get().types);
     }
 
     public Key addKey(String name, PsiElement definition)
@@ -180,7 +180,7 @@ public class DeepType extends Lang
                 }
                 mergedKeys.get(k).addAll(v.getTypes());
             });
-            t.getElemTypes().forEach(indexTypes::add);
+            t.getListElemTypes().forEach(indexTypes::add);
             briefTypes.add(opt(t.stringValue).def(t.briefType.filterUnknown().toString()));
         });
 

src/org/klesun/deep_assoc_completion/helpers/MultiType.java
@@ -47,7 +47,7 @@ public class MultiType extends Lang
     public DeepType getInArray(PsiElement call)
     {
         DeepType result = new DeepType(call, PhpType.ARRAY);
-        result.listElTypes.addAll(types);
+        result.listElTypes.add(() -> new MultiType(types));
         return result;
     }
 
@@ -79,16 +79,16 @@ public class MultiType extends Lang
                 .fap(v -> v.getTypes())
                 .fch(t -> keyTypes.add(t));
             if (Tls.isNum(keyName)) {
-                types.fap(t -> t.listElTypes).fch(t -> keyTypes.add(t));
+                types.fap(t -> t.listElTypes).fch(t -> keyTypes.addAll(t.get().types));
             }
         } else {
             // if keyName is a var - return types of all keys
             types.fap(t -> L(t.keys.values()))
                 .fap(v -> v.getTypes())
                 .fch(t -> keyTypes.add(t));
-            types.fap(t -> t.listElTypes).fch(t -> keyTypes.add(t));
+            types.fap(t -> t.listElTypes).fch(t -> keyTypes.addAll(t.get().types));
         }
-        types.fap(t -> t.anyKeyElTypes).fch(t -> keyTypes.add(t));
+        types.fap(t -> t.anyKeyElTypes).fch(t -> keyTypes.addAll(t.get().types));
         /** @param Segment{} $segment */
         types.fop(t -> opt(t.briefType.elementType().filterUnknown().filterMixed())
             .flt(it -> !it.isEmpty())
@@ -160,7 +160,7 @@ public class MultiType extends Lang
                 .map(s -> (types.all((t,i) -> t.definition.getText().equals(s)))
                     ? s : "'" + s + "'")));
         }
-        if (types.any(t -> t.getElemTypes().size() > 0)) {
+        if (types.any(t -> t.getListElemTypes().size() > 0)) {
             briefValues.add("[" + getEl().getBriefValueText(maxLen, circularRefs) + "]");
         }
         if (briefValues.isEmpty() && types.size() > 0) {

tests/src/Lib/UnitTest.php
@@ -260,7 +260,7 @@ class UnitTest
             ])]),
         ]);
         $sample = ParamUtil::sample($schema);
-        $sample['passengers'][0][''];
+        $sample['passengers'][0]['fullName'];
         // must not reach the 10000 expression limit
         $list[] = [$sample['passengers'][0], ['absoluteNumber' => [], 'fullName' => []]];
         $list[] = [$sample['segments'][0], ['airline' => [], 'departureAirport' => []]];

src/org/klesun/deep_assoc_completion/completion_providers/ArrayColumnPvdr.java
@@ -3,7 +3,6 @@ package org.klesun.deep_assoc_completion.completion_providers;
 import com.intellij.codeInsight.completion.*;
 import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
-import com.intellij.openapi.project.Project;
 import com.intellij.util.ProcessingContext;
 import com.jetbrains.php.lang.psi.elements.*;
 import com.jetbrains.php.lang.psi.elements.impl.FunctionReferenceImpl;
@@ -48,7 +47,7 @@ public class ArrayColumnPvdr extends CompletionProvider<CompletionParameters>
                 suggested.add(key);
                 result.add(makeLookup(keyEntry));
             }
-            L(type.getElemTypes()).gat(0).flt(t -> type.keys.size() == 0).thn(t -> {
+            L(type.getListElemTypes()).gat(0).flt(t -> type.keys.size() == 0).thn(t -> {
                 for (int k = 0; k < 5; ++k) {
                     result.add(makeLookupBase(k + "", t.briefType.toString()));
                 }

src/org/klesun/deep_assoc_completion/completion_providers/DeepKeysPvdr.java
@@ -173,7 +173,7 @@ public class DeepKeysPvdr extends CompletionProvider<CompletionParameters>
             String briefTypeRaw = mt.getKeyBriefType(keyName).filterUnknown().toStringResolved();
             mutLookup.lookupData = makePaddedLookup(keyName, briefTypeRaw, "");
         });
-        L<DeepType> indexTypes = mt.types.fap(t -> t.getElemTypes());
+        L<DeepType> indexTypes = mt.types.fap(t -> t.getListElemTypes());
         if (indexTypes.size() > 0) {
             String typeText = new MultiType(indexTypes).getBriefValueText(BRIEF_TYPE_MAX_LEN);
             String ideaType = new MultiType(indexTypes).getIdeaType().filterUnknown().toStringResolved();

src/org/klesun/deep_assoc_completion/entry/RunTest.java
@@ -56,7 +56,7 @@ public class RunTest extends AnAction
                         return funcCtx.findExprType(retVal).types;
                     })
                     .fap(a -> a)
-                    .fap(ltype -> L(ltype.getElemTypes())
+                    .fap(ltype -> L(ltype.getListElemTypes())
                         .fop((rett, i) -> {
                             CaseContext ctx = new CaseContext(logger);
                             ctx.dataProviderName = func.getName();

src/org/klesun/deep_assoc_completion/resolvers/ArrCtorRes.java
@@ -138,7 +138,7 @@ public class ArrCtorRes extends Lang
                                 .addKey(key, ctx.getRealPsi(keyRec))
                                 .addType(getType, Tls.getIdeaType(v)));
                         } else {
-                            arrayType.anyKeyElTypes.addAll(getType.get().types);
+                            arrayType.anyKeyElTypes.add(getType);
                         }
                     });
             }));

src/org/klesun/deep_assoc_completion/resolvers/FuncCallRes.java
@@ -69,25 +69,23 @@ public class FuncCallRes extends Lang
                         .uni(argArr -> ctx.subCtxSingleArgArr(argArr),
                             () -> new FuncCtx(ctx.getSearch())
                         );
-                    findPsiExprType(callback).types
-                        .map(t -> t.getReturnTypes(subCtx))
-                        .fch(rts -> {
-                            MultiType rmt = new MultiType(rts);
-                            arrMt.types.fch(t -> {
-                                DeepType mapped = new DeepType(t.definition, PhpType.ARRAY);
-                                t.keys.forEach((k,v) -> mapped.addKey(k, v.definition)
-                                    .addType(() -> rmt, rmt.getIdeaType()));
-                                result.add(mapped);
-                            });
-                            if (arrMt.hasNumberIndexes()) {
-                                mapRetType.listElTypes.addAll(rts);
-                            } else if (
-                                arrMt.getKeyNames().size() == 0 ||
-                                arrMt.types.any(t -> t.anyKeyElTypes.size() > 0)
-                            ) {
-                                mapRetType.anyKeyElTypes.addAll(rts);
-                            }
-                        });
+                    S<MultiType> getElMt = () -> findPsiExprType(callback).types
+                        .fap(t -> t.getReturnTypes(subCtx))
+                        .wap(MultiType::new);
+                    arrMt.types.fch(t -> {
+                        DeepType mapped = new DeepType(t.definition, PhpType.ARRAY);
+                        t.keys.forEach((k, v) -> mapped.addKey(k, v.definition)
+                            .addType(getElMt, call.getType().elementType()));
+                        result.add(mapped);
+                    });
+                    if (arrMt.hasNumberIndexes()) {
+                        mapRetType.listElTypes.add(getElMt);
+                    } else if (
+                        arrMt.getKeyNames().size() == 0 ||
+                        arrMt.types.any(t -> t.anyKeyElTypes.size() > 0)
+                    ) {
+                        mapRetType.anyKeyElTypes.add(getElMt);
+                    }
                 }
                 result.add(mapRetType);
             } else if (name.equals("array_filter") || name.equals("array_reverse")
@@ -102,14 +100,17 @@ public class FuncCallRes extends Lang
                     .map(p -> findPsiExprType(p))
                     .fap(mt -> mt.getEl().types)
                     .gop(t -> opt(t.stringValue));
-                MultiType elMt = L(params).gat(1)
+                S<MultiType> getElMt = () -> L(params).gat(1)
                     .map(p -> findPsiExprType(p))
                     .def(MultiType.INVALID_PSI)
                     .getEl();
+                PhpType ideaElType = L(params).gat(1)
+                    .fop(toCast(PhpExpression.class))
+                    .map(e -> e.getType()).def(PhpType.MIXED);
                 keyToTypes.fch((types,keyName) -> combine
                     .addKey(keyName, types.map(t -> t.definition).fst().def(call))
-                        .addType(() -> elMt, elMt.getIdeaType()));
-                combine.anyKeyElTypes.addAll(elMt.types);
+                        .addType(getElMt, ideaElType));
+                combine.anyKeyElTypes.add(getElMt);
                 result.add(combine);
             } else if (name.equals("array_pop") || name.equals("array_shift")
                     || name.equals("current") || name.equals("end") || name.equals("next")
@@ -126,14 +127,11 @@ public class FuncCallRes extends Lang
                     Tls.cast(PhpExpression.class, params[1])
                         .map(keyNamePsi -> ctx.findExprType(keyNamePsi))
                         .map(mt -> opt(mt.getStringValue()))
-                        .map(keyName -> elType.getKey(keyName.def(null)).types)
-                        .flt(itypes -> itypes.size() > 0)
-                        .map(itypes -> {
+                        .thn(keyName -> {
                             DeepType type = new DeepType(call);
-                            type.listElTypes.addAll(itypes);
-                            return list(type);
-                        })
-                        .thn(result::addAll);
+                            type.listElTypes.add(() -> elType.getKey(keyName.def(null)));
+                            result.add(type);
+                        });
                 }
             } else if (name.equals("array_chunk")) {
                 L(params).gat(0)

commit d31e11ebf57aa151fa49e22ee8ef69f1df76aa2b

#45 Fix StackOverflowException on array_combine expressed through itself
Caused by 1f8bd03

src/org/klesun/deep_assoc_completion/DeepType.java
@@ -155,10 +155,10 @@ public class DeepType extends Lang
         return new String(new char[level]).replace("\0", "  ");
     }
 
-    public static String varExport(List<DeepType> types, int level)
+    public static String varExport(List<DeepType> types)
     {
         Set<DeepType> circularRefs = new HashSet<>();
-        return varExport(types, level, circularRefs);
+        return varExport(types, 0, circularRefs);
     }
 
     public static String varExport(List<DeepType> types, int level, Set<DeepType> circularRefs)
@@ -211,7 +211,7 @@ public class DeepType extends Lang
     @Override
     public String toString()
     {
-        return varExport(list(this), 0);
+        return varExport(list(this));
     }
 
     public boolean hasNumberIndexes()

src/org/klesun/deep_assoc_completion/entry/ShowDocs.java
@@ -62,7 +62,7 @@ public class ShowDocs extends AnAction
             .fop(psiFile -> opt(e.getData(LangDataKeys.CARET))
                 .map(caret -> psiFile.findElementAt(caret.getOffset())))
             .map(psi -> psi instanceof LeafPsiElement ? psi.getParent() : psi)
-            .map(psi -> DeepType.varExport(findPsiType(psi), 0))
+            .map(psi -> DeepType.varExport(findPsiType(psi)))
             .def("There is no file opened to describe a php variable");
 
         System.out.println(doc);

src/org/klesun/deep_assoc_completion/helpers/FuncCtx.java
@@ -57,7 +57,9 @@ public class FuncCtx extends Lang
     private MultiType getCached(int index, S<MultiType> argGetter)
     {
         if (!cachedArgs.containsKey(index)) {
-            cachedArgs.put(index, argGetter.get());
+            cachedArgs.put(index, MultiType.CIRCULAR_REFERENCE);
+            MultiType mt = argGetter.get();
+            cachedArgs.replace(index, mt);
         }
         return cachedArgs.get(index);
     }

src/org/klesun/deep_assoc_completion/helpers/MultiType.java
@@ -27,6 +27,8 @@ public class MultiType extends Lang
     private REASON reason;
     final public L<DeepType> types;
 
+    private boolean isGettingKey = false;
+
     public MultiType(L<DeepType> types, REASON reason)
     {
         // there sometimes appear thousands of duplicates
@@ -72,6 +74,11 @@ public class MultiType extends Lang
 
     public MultiType getKey(String keyName)
     {
+        if (isGettingKey) { // see issue #45
+            return MultiType.CIRCULAR_REFERENCE;
+        }
+        isGettingKey = true;
+
         L<DeepType> keyTypes = list();
         if (keyName != null) {
             // if keyName is a constant string - return type of this key only
@@ -89,11 +96,12 @@ public class MultiType extends Lang
             types.fap(t -> t.listElTypes).fch(t -> keyTypes.addAll(t.get().types));
         }
         types.fap(t -> t.anyKeyElTypes).fch(t -> keyTypes.addAll(t.get().types));
-        /** @param Segment{} $segment */
         types.fop(t -> opt(t.briefType.elementType().filterUnknown().filterMixed())
             .flt(it -> !it.isEmpty())
             .map(it -> new DeepType(t.definition, it, false)))
             .fch(t -> keyTypes.add(t));
+
+        isGettingKey = false;
         return new MultiType(keyTypes);
     }
 
@@ -183,7 +191,7 @@ public class MultiType extends Lang
 
     public String varExport()
     {
-        return DeepType.varExport(types, 0);
+        return DeepType.varExport(types);
     }
 
     public boolean hasNumberIndexes()

src/org/klesun/deep_assoc_completion/helpers/SearchContext.java
@@ -168,6 +168,7 @@ public class SearchContext extends Lang
                 System.out.println(indent + "<< TAKING RESULT FROM CACHE");
             }
         } else {
+            putToCache(funcCtx, expr, MultiType.CIRCULAR_REFERENCE);
             result = DeepTypeResolver.resolveIn(expr, funcCtx)
                 .map(ts -> new MultiType(ts));
             result.thn(mt -> putToCache(funcCtx, expr, mt));

tests/src/DeepTest/UnitTest.php
@@ -2017,6 +2017,28 @@ class UnitTest implements IProcessPntQueueAction /** extends \PHPUnit_Framework_
         return $list;
     }
 
+    private static function getRecursedArrayCombine()
+    {
+        if (rand() % 2) {
+            $values = static::getRecursedArrayCombine();
+        } else {
+            $values = [['cute' => 1], ['popular' => 1], ['strong' => 1]];
+        }
+        return array_combine(
+            ['pichu', 'pikachu', 'raichu'],
+            $values
+        );
+    }
+
+    public function provideStackOverflowArrayCombine()
+    {
+        $list = [];
+        $combined = static::getRecursedArrayCombine();
+        $combined['pichu']['strong'];
+        $list[] = [$combined['raichu'], ['strong' => []]];
+        return $list;
+    }
+
     //=============================
     // following are not implemented yet
     //=============================