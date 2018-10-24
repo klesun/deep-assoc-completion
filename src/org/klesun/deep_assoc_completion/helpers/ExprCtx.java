klesun/deep-assoc-completion

commit a69263fc0dcf5c090a0372a4990b83af41957fb2

Refactor: keep track of parent expressions in a tree

src/org/klesun/deep_assoc_completion/helpers/ExprCtx.java
@@ -1,0 +1,104 @@
+package org.klesun.deep_assoc_completion.helpers;
+
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.jetbrains.php.lang.psi.elements.FunctionReference;
+import com.jetbrains.php.lang.psi.elements.NewExpression;
+import com.jetbrains.php.lang.psi.elements.ParameterListOwner;
+import com.jetbrains.php.lang.psi.elements.PhpExpression;
+import com.jetbrains.php.lang.psi.elements.impl.FieldReferenceImpl;
+import com.jetbrains.php.lang.psi.resolve.types.PhpType;
+import org.jetbrains.annotations.Nullable;
+import org.klesun.deep_assoc_completion.DeepType;
+import org.klesun.lang.It;
+import org.klesun.lang.*;
+
+import java.util.Collection;
+import java.util.Map;
+
+import static org.klesun.lang.Lang.*;
+
+/**
+ * like FuncCtx, but points to the parent _expression_ not the parent _function_
+ * I'm going to use this to keep record of _expression tree_ for depth limits and debug
+ */
+public class ExprCtx implements IExprCtx {
+    final private int depth;
+    final private FuncCtx funcCtx;
+    final private PsiElement expr;
+    final private L<ExprCtx> children = list();
+
+    public ExprCtx(FuncCtx funcCtx, PsiElement expr, int depth) {
+        this.funcCtx = funcCtx;
+        this.expr = expr;
+        this.depth = depth;
+    }
+
+    public IFuncCtx func() {
+        return funcCtx;
+    }
+
+    private ExprCtx subExpr(PsiElement expr, FuncCtx funcCtx) {
+        ExprCtx nextCtx = new ExprCtx(funcCtx, expr, depth + 1);
+        children.add(nextCtx);
+        return nextCtx;
+    }
+
+    public ExprCtx subCtxEmpty() {
+        return subExpr(expr, funcCtx.subCtxEmpty());
+    }
+
+    public ExprCtx subCtxEmpty(PsiElement fakeFileSource) {
+        FuncCtx funcSubCtx = funcCtx.subCtxEmpty();
+        funcSubCtx.fakeFileSource = som(fakeFileSource);
+        return subExpr(expr, funcSubCtx);
+    }
+
+    public ExprCtx subCtxDirect(FunctionReference funcCall) {
+        return subExpr(expr, funcCtx.subCtxDirect(funcCall, this::findExprType));
+    }
+
+    public ExprCtx subCtxDirect(NewExpression funcCall) {
+        return subExpr(expr, funcCtx.subCtxDirect(funcCall, this::findExprType));
+    }
+
+    public ExprCtx subCtxDirectGeneric(ParameterListOwner funcCall) {
+        return subExpr(expr, funcCtx.subCtxDirectGeneric(funcCall, this::findExprType));
+    }
+
+    public ExprCtx subCtxSingleArgArr(PhpExpression argArr) {
+        return subExpr(expr, funcCtx.subCtxSingleArgArr(argArr, this::findExprType));
+    }
+
+    public ExprCtx subCtxIndirect(PhpExpression args) {
+        return subExpr(expr, funcCtx.subCtxIndirect(args, this::findExprType));
+    }
+
+    public Opt<PsiElement> getFakeFileSource() {
+        return funcCtx.fakeFileSource;
+    }
+
+    public Map<PsiFile, Collection<FieldReferenceImpl>> getFieldRefCache() {
+        return funcCtx.getSearch().fileToFieldRefs;
+    }
+
+    public It<DeepType> getThisType() {
+        return funcCtx.instGetter.fap(g -> g.get().types);
+    }
+
+    public Opt<PhpType> getSelfType() {
+        return funcCtx.clsIdeaType;
+    }
+
+    public It<DeepType> findExprType(PhpExpression expr) {
+        return It(funcCtx.getSearch().findExprType(expr, subExpr(expr, funcCtx)));
+    }
+
+    public It<DeepType> limitResolve(int limit, PhpExpression expr) {
+        int oldDepth = funcCtx.getSearch().depthLeft;
+        SearchContext keySearch = new SearchContext(funcCtx.getSearch().project.def(null));
+        keySearch.setDepth(Math.min(oldDepth, limit));
+        keySearch.overrideMaxExpr = som(Math.min(funcCtx.getSearch().getMaxExpressions() - funcCtx.getSearch().getExpressionsResolved() - 1, limit));
+        return It(keySearch.findExprType(expr, subExpr(expr, new FuncCtx(keySearch))));
+    }
+}

src/org/klesun/deep_assoc_completion/helpers/FuncCtx.java
@@ -15,7 +15,7 @@ import java.util.HashMap;
 import java.util.Set;
 
 /** a node in called function stack trace with args */
-public class FuncCtx extends Lang
+public class FuncCtx extends Lang implements IFuncCtx
 {
     enum EArgPsiType {DIRECT, ARR, NONE, INDIRECT};
 
@@ -111,60 +111,43 @@ public class FuncCtx extends Lang
         }
     }
 
-    public Opt<Mt> getArg(Integer index)
-    {
-        return getArg(new ArgOrder(index, false));
-    }
-
-    public Mt getArgMt(Integer index)
-    {
-        return getArg(new ArgOrder(index, false)).def(Mt.INVALID_PSI);
-    }
-
-    @NotNull
+    /** should be called _only_ from an entry level class */
     public It<DeepType> findExprType(PhpExpression expr)
     {
-        return It(search.findExprType(expr, this));
+        IExprCtx exprCtx = new ExprCtx(this, expr, 0);
+        return It(search.findExprType(expr, exprCtx));
     }
 
-    public It<DeepType> limitResolve(int limit, PhpExpression expr)
-    {
-        int oldDepth = search.depthLeft;
-        search.setDepth(Math.min(oldDepth, limit));
-        It<DeepType> result = findExprType(expr);
-        search.setDepth(oldDepth);
-        return result;
-    }
 
-    public FuncCtx subCtxDirect(FunctionReference funcCall)
+    public FuncCtx subCtxDirect(FunctionReference funcCall, F<PhpExpression, It<DeepType>> findExprType)
     {
-        FuncCtx self = subCtxDirectGeneric(funcCall);
+        FuncCtx self = subCtxDirectGeneric(funcCall, findExprType);
         Tls.cast(MethodReference.class, funcCall)
             .map(methCall -> methCall.getClassReference())
             .thn(ref -> {
                 if (ref instanceof ClassReference) {
                     self.clsIdeaType = opt(ref.getType());
                 } else {
-                    self.instGetter = opt(() -> new Mt(findExprType(ref)));
+                    self.instGetter = opt(() -> new Mt(findExprType.apply(ref)));
                 }
             });
         return self;
     }
 
-    public FuncCtx subCtxDirect(NewExpression funcCall)
+    public FuncCtx subCtxDirect(NewExpression funcCall, F<PhpExpression, It<DeepType>> findExprType)
     {
-        FuncCtx self = subCtxDirectGeneric(funcCall);
+        FuncCtx self = subCtxDirectGeneric(funcCall, findExprType);
         opt(funcCall.getClassReference())
             .thn(ref -> self.clsIdeaType = opt(ref.getType()));
         return self;
     }
 
-    public FuncCtx subCtxDirectGeneric(ParameterListOwner funcCall)
+    public FuncCtx subCtxDirectGeneric(ParameterListOwner funcCall, F<PhpExpression, It<DeepType>> findExprType)
     {
         L<PsiElement> psiArgs = L(funcCall.getParameters());
         L<S<Mt>> argGetters = psiArgs.map((psi) -> S(() ->
             Tls.cast(PhpExpression.class, psi) .uni(
-                arg -> new Mt(findExprType(arg)),
+                arg -> new Mt(findExprType.apply(arg)),
                 () -> Mt.INVALID_PSI)
         )).arr();
         FuncCtx subCtx = new FuncCtx(this, argGetters, funcCall, EArgPsiType.DIRECT);
@@ -177,16 +160,16 @@ public class FuncCtx extends Lang
     }
 
     /** context from args passed in array for example in array_map or array_filter */
-    public FuncCtx subCtxSingleArgArr(PhpExpression argArr)
+    public FuncCtx subCtxSingleArgArr(PhpExpression argArr, F<PhpExpression, It<DeepType>> findExprType)
     {
-        L<S<Mt>> argGetters = list(() -> findExprType(argArr).fap(Mt::getElSt).wap(Mt::new));
+        L<S<Mt>> argGetters = list(() -> findExprType.apply(argArr).fap(Mt::getElSt).wap(Mt::new));
         return new FuncCtx(this, argGetters, argArr, EArgPsiType.ARR);
     }
 
     /** when you have expression PSI and it is not directly passed to the func, ex. call_user_func_array() */
-    public FuncCtx subCtxIndirect(PhpExpression args)
+    public FuncCtx subCtxIndirect(PhpExpression args, F<PhpExpression, It<DeepType>> findExprType)
     {
-        S<Mt> getMt = Tls.onDemand(() -> new Mt(findExprType(args)));
+        S<Mt> getMt = Tls.onDemand(() -> new Mt(findExprType.apply(args)));
         L<S<Mt>> argGetters = list();
         // always 10 arguments, got any problem?
         // it probably should be done correctly one day...

src/org/klesun/deep_assoc_completion/helpers/IExprCtx.java
@@ -1,0 +1,44 @@
+package org.klesun.deep_assoc_completion.helpers;
+
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.jetbrains.php.lang.psi.elements.FunctionReference;
+import com.jetbrains.php.lang.psi.elements.NewExpression;
+import com.jetbrains.php.lang.psi.elements.ParameterListOwner;
+import com.jetbrains.php.lang.psi.elements.PhpExpression;
+import com.jetbrains.php.lang.psi.elements.impl.FieldReferenceImpl;
+import com.jetbrains.php.lang.psi.resolve.types.PhpType;
+import org.klesun.deep_assoc_completion.DeepType;
+import org.klesun.lang.It;
+import org.klesun.lang.Lang;
+import org.klesun.lang.Opt;
+
+import java.util.Collection;
+import java.util.Map;
+
+import static org.klesun.lang.Lang.som;
+
+/**
+ * making it an interface since I'm refactor the context
+ * class passed to resolvers for the third time already
+ *
+ * maybe should rename to just ICtx or IResolutionCtx since
+ * it does not have any expression-specific methods...
+ */
+public interface IExprCtx {
+    IFuncCtx func();
+    IExprCtx subCtxEmpty();
+    IExprCtx subCtxEmpty(PsiElement fakeFileSource);
+    IExprCtx subCtxDirect(FunctionReference funcCall);
+    IExprCtx subCtxDirect(NewExpression funcCall);
+    IExprCtx subCtxDirectGeneric(ParameterListOwner funcCall);
+    IExprCtx subCtxSingleArgArr(PhpExpression argArr);
+    IExprCtx subCtxIndirect(PhpExpression args);
+    It<DeepType> getThisType();
+    Opt<PhpType> getSelfType();
+    Opt<PsiElement> getFakeFileSource();
+    Map<PsiFile, Collection<FieldReferenceImpl>> getFieldRefCache();
+
+    It<DeepType> findExprType(PhpExpression expr);
+    It<DeepType> limitResolve(int limit, PhpExpression expr);
+}

src/org/klesun/deep_assoc_completion/resolvers/ArrCtorRes.java
@@ -10,6 +10,7 @@ import com.jetbrains.php.lang.psi.elements.impl.ClassReferenceImpl;
 import com.jetbrains.php.lang.psi.resolve.types.PhpType;
 import org.klesun.deep_assoc_completion.DeepType;
 import org.klesun.deep_assoc_completion.helpers.FuncCtx;
+import org.klesun.deep_assoc_completion.helpers.IExprCtx;
 import org.klesun.deep_assoc_completion.helpers.KeyType;
 import org.klesun.deep_assoc_completion.helpers.Mt;
 import org.klesun.lang.*;
@@ -21,9 +22,9 @@ import java.util.Set;
 
 public class ArrCtorRes extends Lang
 {
-    final private FuncCtx ctx;
+    final private IExprCtx ctx;
 
-    public ArrCtorRes(FuncCtx ctx)
+    public ArrCtorRes(IExprCtx ctx)
     {
         this.ctx = ctx;
     }
@@ -133,7 +134,7 @@ public class ArrCtorRes extends Lang
                 // currently each value is wrapped into a plane Psi element
                 // i believe this is likely to change in future - so we try both cases
                 .elf(() -> opt(valuePsi.getFirstChild()).fop(toCast(PhpExpression.class)))
-                .thn(val -> arrayType.addKey(i + "", ctx.getRealPsi(val))
+                .thn(val -> arrayType.addKey(i + "", ctx.getFakeFileSource().def(val))
                     .addType(() -> ctx.findExprType(val).wap(Mt::new), Tls.getIdeaType(val))));
 
         // keyed elements
@@ -148,7 +149,7 @@ public class ArrCtorRes extends Lang
                     .thn(keyStrValues -> {
                         if (keyStrValues.has()) {
                             keyStrValues.fch(key -> arrayType
-                                .addKey(key, ctx.getRealPsi(keyRec))
+                                .addKey(key, ctx.getFakeFileSource().def(keyRec))
                                 .addType(getType, Tls.getIdeaType(v)));
                         } else {
                             arrayType.addKey(KeyType.unknown(keyRec)).addType(getType);

src/org/klesun/deep_assoc_completion/resolvers/MethCallRes.java
@@ -11,6 +11,8 @@ import com.jetbrains.php.lang.psi.elements.impl.MethodReferenceImpl;
 import com.jetbrains.php.lang.psi.resolve.types.PhpType;
 import org.klesun.deep_assoc_completion.DeepType;
 import org.klesun.deep_assoc_completion.helpers.FuncCtx;
+import org.klesun.deep_assoc_completion.helpers.IExprCtx;
+import org.klesun.deep_assoc_completion.helpers.IFuncCtx;
 import org.klesun.deep_assoc_completion.helpers.Mt;
 import org.klesun.deep_assoc_completion.resolvers.var_res.DocParamRes;
 import org.klesun.lang.*;
@@ -22,9 +24,9 @@ import java.util.regex.Pattern;
 
 public class MethCallRes extends Lang
 {
-    final private FuncCtx ctx;
+    final private IExprCtx ctx;
 
-    public MethCallRes(FuncCtx ctx)
+    public MethCallRes(IExprCtx ctx)
     {
         this.ctx = ctx;
     }
@@ -85,7 +87,7 @@ public class MethCallRes extends Lang
                             .fap(a -> list(a));
                     }
                 });
-        }).fch(name -> parsedType.addKey(name, ctx.getRealPsi(strType.definition))
+        }).fch(name -> parsedType.addKey(name, ctx.getFakeFileSource().def(strType.definition))
             .addType(() -> new Mt(list(new DeepType(strType.definition, PhpType.STRING))), PhpType.STRING));
         return parsedType;
     }
@@ -108,7 +110,7 @@ public class MethCallRes extends Lang
         });
     }
 
-    private It<DeepType> findBuiltInRetType(Method meth, FuncCtx argCtx, MethodReference methCall)
+    private It<DeepType> findBuiltInRetType(Method meth, IFuncCtx argCtx, MethodReference methCall)
     {
         It<DeepType> types = It(list());
         String clsNme = opt(meth.getContainingClass()).map(cls -> cls.getName()).def("");
@@ -134,27 +136,26 @@ public class MethCallRes extends Lang
         return types;
     }
 
-    private static It<DeepType> parseReturnDoc(PhpDocReturnTag returnDoc, FuncCtx funcCtx)
+    private static It<DeepType> parseReturnDoc(PhpDocReturnTag returnDoc, IExprCtx funcCtx)
     {
-        FuncCtx docCtx = new FuncCtx(funcCtx.getSearch());
-        docCtx.fakeFileSource = opt(returnDoc);
+        IExprCtx docCtx = funcCtx.subCtxEmpty(returnDoc);
         return Tls.regex("^\\s*(like\\s*|=|)((?:\\[|[a-zA-Z]+[\\(:]|new\\s+).*)$", returnDoc.getTagValue())
             .fop(match -> match.gat(1))
             .fop(expr -> DocParamRes.parseExpression(expr, returnDoc.getProject(), docCtx))
             .def(It.non());
     }
 
-    public static F<FuncCtx, It<DeepType>> findMethRetType(Method meth)
+    public static F<IExprCtx, It<DeepType>> findMethRetType(Method meth)
     {
-        return (FuncCtx funcCtx) -> {
+        return (IExprCtx funcCtx) -> {
             It<Method> impls = It(list(meth));
             if (meth.isAbstract()) {
                 impls = It.cnc(list(meth), findOverridingMethods(meth));
                 // ignore $this and args in implementations
                 // since there may be dozens of them (Laravel)
-                funcCtx = new FuncCtx(funcCtx.getSearch());
+                funcCtx = funcCtx.subCtxEmpty();
             }
-            FuncCtx finalCtx = funcCtx;
+            IExprCtx finalCtx = funcCtx;
             return impls.fap(m -> It.cnc(
                 ClosRes.getReturnedValue(m, finalCtx),
                 opt(meth.getDocComment()).map(doc -> doc.getReturnTag())
@@ -164,12 +165,12 @@ public class MethCallRes extends Lang
         };
     }
 
-    public static It<Method> resolveMethodsNoNs(MethodReference call, FuncCtx ctx)
+    public static It<Method> resolveMethodsNoNs(MethodReference call, IExprCtx ctx)
     {
         String cls = opt(call.getClassReference()).map(c -> c.getText()).def("");
         String mth = opt(call.getName()).def("");
         if (cls.equals("self") || cls.equals("static")) {
-            cls = ctx.fakeFileSource
+            cls = ctx.getFakeFileSource()
                 .fop(doc -> Tls.findParent(doc, PhpClass.class, a -> true))
                 .map(clsPsi -> clsPsi.getFQN())
                 .def(cls);
@@ -190,12 +191,12 @@ public class MethCallRes extends Lang
             .flt(m -> Objects.equals(m.getName(), func));
     }
 
-    private It<Method> findReferenced(MethodReferenceImpl fieldRef, FuncCtx ctx)
+    private It<Method> findReferenced(MethodReferenceImpl fieldRef, IExprCtx ctx)
     {
         long startTime = System.nanoTime();
         It<Method> mit = opt(fieldRef.getClassReference())
             .fap(obj -> It.frs(
-                () -> ctx.clsIdeaType
+                () -> ctx.getSelfType()
                     // IDEA resolve static:: incorrectly, it either treats it
                     // same as self::, either does not resolve it at all
                     .flt(typ -> obj.getText().equals("static"))
@@ -214,7 +215,7 @@ public class MethCallRes extends Lang
         return mit;
     }
 
-    private Opt<It<Method>> resolveMethodFromCall(MethodReferenceImpl call, FuncCtx ctx)
+    private Opt<It<Method>> resolveMethodFromCall(MethodReferenceImpl call, IExprCtx ctx)
     {
         return Opt.fst(() -> opt(null)
             , () -> opt(findReferenced(call, ctx)).flt(found -> found.has())
@@ -229,12 +230,12 @@ public class MethCallRes extends Lang
 
     public It<DeepType> resolveCall(MethodReferenceImpl funcCall)
     {
-        FuncCtx funcCtx = ctx.subCtxDirect(funcCall);
+        IExprCtx funcCtx = ctx.subCtxDirect(funcCall);
         return resolveMethodFromCall(funcCall, ctx)
             .fap(funcs -> funcs)
             .fap(func -> It.cnc(
                 findMethRetType(func).apply(funcCtx),
-                findBuiltInRetType(func, funcCtx, funcCall)
+                findBuiltInRetType(func, funcCtx.func(), funcCall)
             ));
     }
 }

resources/META-INF/plugin.xml
@@ -1,7 +1,7 @@
 <idea-plugin>
     <id>lv.midiana.misc.phpstorm-plugins.deep-keys</id>
     <name>deep-assoc-completion</name>
-    <version>2018.10.19.001</version>
+    <version>2018.10.21.001</version>
     <vendor email="safronevev@gmail.com" url="http://midiana.lv/entry/phpstorm-deep-keys">Klesun</vendor>
 
     <description><![CDATA[

src/org/klesun/deep_assoc_completion/DeepType.java
@@ -8,9 +8,7 @@ import com.jetbrains.php.lang.psi.elements.impl.PhpExpressionImpl;
 import com.jetbrains.php.lang.psi.elements.impl.StringLiteralExpressionImpl;
 import com.jetbrains.php.lang.psi.resolve.types.PhpType;
 import org.jetbrains.annotations.Nullable;
-import org.klesun.deep_assoc_completion.helpers.FuncCtx;
-import org.klesun.deep_assoc_completion.helpers.KeyType;
-import org.klesun.deep_assoc_completion.helpers.Mt;
+import org.klesun.deep_assoc_completion.helpers.*;
 import org.klesun.lang.*;
 
 import java.util.*;
@@ -28,10 +26,10 @@ public class DeepType extends Lang
     // (starting with self::) and [$obj, 'functionName'] tuples
     // slowly migrating returnTypes from constant values to a function
     // list of functions that take arg list and return list of return types
-    public final L<F<FuncCtx, MemoizingIterable<DeepType>>> returnTypeGetters = L();
+    public final L<F<IExprCtx, MemoizingIterable<DeepType>>> returnTypeGetters = L();
     public final L<DeepType> pdoFetchTypes = L();
     public final LinkedHashSet<String> pdoBindVars = new LinkedHashSet<>();
-    public Opt<FuncCtx> ctorArgs = opt(null);
+    public Opt<IExprCtx> ctorArgs = opt(null);
     public Opt<PhpType> clsRefType = non();
     public final @Nullable String stringValue;
     public final PsiElement definition;
@@ -80,7 +78,7 @@ public class DeepType extends Lang
     }
 
     /** new object creation */
-    public static DeepType makeNew(NewExpression newExp, FuncCtx ctorArgs, PhpType ideaType)
+    public static DeepType makeNew(NewExpression newExp, IExprCtx ctorArgs, PhpType ideaType)
     {
         DeepType self = new DeepType(newExp, ideaType, null);
         self.ctorArgs = opt(ctorArgs);
@@ -95,7 +93,7 @@ public class DeepType extends Lang
         return self;
     }
 
-    public It<DeepType> getReturnTypes(FuncCtx ctx)
+    public It<DeepType> getReturnTypes(IExprCtx ctx)
     {
         return returnTypeGetters.fap(g -> g.apply(ctx));
     }
@@ -234,9 +232,6 @@ public class DeepType extends Lang
             It<String> briefs = It(new HashSet<>(briefTypes)).flt(t -> !"".equals(t));
             result = "'" + Tls.implode("|", briefs) + "'";
         }
-        result += It(types).fop(t -> t.ctorArgs)
-            .map(ctx -> Tls.implode(", ", ctx.getArgs().map(a -> a.varExport()))).arr()
-            .grp(a -> a).kys().fst().map(argStr -> "(" + argStr + ")").def("");
         circularRefs.removeAll(types);
         return result;
     }

src/org/klesun/deep_assoc_completion/DeepTypeResolver.java
@@ -6,18 +6,22 @@ import com.jetbrains.php.lang.psi.elements.ClassConstantReference;
 import com.jetbrains.php.lang.psi.elements.ParenthesizedExpression;
 import com.jetbrains.php.lang.psi.elements.PhpExpression;
 import com.jetbrains.php.lang.psi.elements.impl.*;
+import org.klesun.deep_assoc_completion.helpers.ExprCtx;
 import org.klesun.deep_assoc_completion.helpers.FuncCtx;
+import org.klesun.deep_assoc_completion.helpers.IExprCtx;
 import org.klesun.deep_assoc_completion.resolvers.*;
 import org.klesun.lang.*;
+
+import static org.klesun.lang.Lang.*;
 
 /**
  * Provides mechanism to determine expression type.
  * Unlike original jetbrain's type resolver, this
  * includes associative array key information
  */
-public class DeepTypeResolver extends Lang
+public class DeepTypeResolver
 {
-    private static It<DeepType> resolveClsConst(ClassConstantReference cst, FuncCtx ctx)
+    private static It<DeepType> resolveClsConst(ClassConstantReference cst, IExprCtx ctx)
     {
         if ("class".equals(cst.getName())) {
             return opt(cst.getClassReference())
@@ -29,11 +33,11 @@ public class DeepTypeResolver extends Lang
                 .fop(toCast(ClassConstImpl.class))
                 .map(a -> a.getDefaultValue())
                 .fop(toCast(PhpExpression.class))
-                .fap(exp -> new FuncCtx(ctx.getSearch()).findExprType(exp));
+                .fap(exp -> ctx.subCtxEmpty().findExprType(exp));
         }
     }
 
-    public static It<DeepType> resolveIn(PhpExpression expr, FuncCtx ctx)
+    public static It<DeepType> resolveIn(PhpExpression expr, IExprCtx ctx)
     {
         It<DeepType> tit = It.frs(() -> It.non()
             , () -> Tls.cast(VariableImpl.class, expr)

commit fff5857b80b367d3140e42607ff5e21bd833a5b7

Fix: Go To the doc instead of expression _only_ if we get to the foo.bar file otherwise
Was accidentally broken in a69263fc0dcf5c090a0372a4990b83af41957fb2

src/org/klesun/deep_assoc_completion/helpers/ExprCtx.java
@@ -63,10 +63,6 @@ public class ExprCtx implements IExprCtx {
         return subExpr(expr, funcCtx.subCtxDirect(funcCall, this::findExprType));
     }
 
-    public ExprCtx subCtxDirectGeneric(ParameterListOwner funcCall) {
-        return subExpr(expr, funcCtx.subCtxDirectGeneric(funcCall, this::findExprType));
-    }
-
     public ExprCtx subCtxSingleArgArr(PhpExpression argArr) {
         return subExpr(expr, funcCtx.subCtxSingleArgArr(argArr, this::findExprType));
     }

src/org/klesun/deep_assoc_completion/helpers/FuncCtx.java
@@ -257,20 +257,4 @@ public class FuncCtx extends Lang implements IFuncCtx
     {
         return getCallStack().size();
     }
-
-    /**
-     * when you parse text, attempts to go to a PSI
-     * in it will lead you to a fake foo.bar file
-     * I would rather go to the doc
-     */
-    public PsiElement getRealPsi(PsiElement maybeFake)
-    {
-        PsiFile file = maybeFake.getContainingFile();
-        PsiDirectory dir = file.getContainingDirectory();
-        if (dir == null && fakeFileSource.has()) {
-            return fakeFileSource.unw();
-        } else {
-            return maybeFake;
-        }
-    }
 }

src/org/klesun/deep_assoc_completion/helpers/IExprCtx.java
@@ -1,5 +1,6 @@
 package org.klesun.deep_assoc_completion.helpers;
 
+import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.jetbrains.php.lang.psi.elements.FunctionReference;
@@ -31,7 +32,6 @@ public interface IExprCtx {
     IExprCtx subCtxEmpty(PsiElement fakeFileSource);
     IExprCtx subCtxDirect(FunctionReference funcCall);
     IExprCtx subCtxDirect(NewExpression funcCall);
-    IExprCtx subCtxDirectGeneric(ParameterListOwner funcCall);
     IExprCtx subCtxSingleArgArr(PhpExpression argArr);
     IExprCtx subCtxIndirect(PhpExpression args);
     It<DeepType> getThisType();
@@ -41,4 +41,21 @@ public interface IExprCtx {
 
     It<DeepType> findExprType(PhpExpression expr);
     It<DeepType> limitResolve(int limit, PhpExpression expr);
+
+
+    /**
+     * when you parse text, attempts to go to a PSI
+     * in it will lead you to a fake foo.bar file
+     * I would rather go to the doc
+     */
+    default PsiElement getRealPsi(PsiElement maybeFake)
+    {
+        PsiFile file = maybeFake.getContainingFile();
+        PsiDirectory dir = file.getContainingDirectory();
+        if (dir == null && getFakeFileSource().has()) {
+            return getFakeFileSource().unw();
+        } else {
+            return maybeFake;
+        }
+    }
 }

src/org/klesun/deep_assoc_completion/resolvers/ArrCtorRes.java
@@ -134,7 +134,7 @@ public class ArrCtorRes extends Lang
                 // currently each value is wrapped into a plane Psi element
                 // i believe this is likely to change in future - so we try both cases
                 .elf(() -> opt(valuePsi.getFirstChild()).fop(toCast(PhpExpression.class)))
-                .thn(val -> arrayType.addKey(i + "", ctx.getFakeFileSource().def(val))
+                .thn(val -> arrayType.addKey(i + "", ctx.getRealPsi(val))
                     .addType(() -> ctx.findExprType(val).wap(Mt::new), Tls.getIdeaType(val))));
 
         // keyed elements
@@ -149,7 +149,7 @@ public class ArrCtorRes extends Lang
                     .thn(keyStrValues -> {
                         if (keyStrValues.has()) {
                             keyStrValues.fch(key -> arrayType
-                                .addKey(key, ctx.getFakeFileSource().def(keyRec))
+                                .addKey(key, ctx.getRealPsi(keyRec))
                                 .addType(getType, Tls.getIdeaType(v)));
                         } else {
                             arrayType.addKey(KeyType.unknown(keyRec)).addType(getType);

src/org/klesun/deep_assoc_completion/resolvers/MethCallRes.java
@@ -87,7 +87,7 @@ public class MethCallRes extends Lang
                             .fap(a -> list(a));
                     }
                 });
-        }).fch(name -> parsedType.addKey(name, ctx.getFakeFileSource().def(strType.definition))
+        }).fch(name -> parsedType.addKey(name, ctx.getRealPsi(strType.definition))
             .addType(() -> new Mt(list(new DeepType(strType.definition, PhpType.STRING))), PhpType.STRING));
         return parsedType;
     }