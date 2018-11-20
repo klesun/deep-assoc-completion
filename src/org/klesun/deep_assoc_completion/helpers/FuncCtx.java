klesun/deep-assoc-completion

commit 2e2d72bce65395b93c9e0777c1814e1d2a0523d4

Support closure use vars

src/org/klesun/deep_assoc_completion/helpers/FuncCtx.java
@@ -26,6 +26,7 @@ public class FuncCtx extends Lang implements IFuncCtx
     private L<Integer> variadicOrders = L();
     public Opt<Lang.S<Mt>> instGetter = opt(null);
     public Opt<PhpType> clsIdeaType = opt(null);
+    public L<T2<String, S<MemoizingIterable<DeepType>>>> closureVars = list();
     final private EArgPsiType argPsiType;
     /** use this when you need to reference a real PSI during parsing of PHP Doc */
     public Opt<PsiElement> fakeFileSource = opt(null);
@@ -190,6 +191,18 @@ public class FuncCtx extends Lang implements IFuncCtx
     public FuncCtx subCtxEmpty()
     {
         return new FuncCtx(this, list(), null, EArgPsiType.NONE);
+    }
+
+    public FuncCtx withClosure(L<T2<String, S<MemoizingIterable<DeepType>>>> closureVars)
+    {
+        FuncCtx closCtx = new FuncCtx(this, argGetters, uniqueRef.def(null), EArgPsiType.INDIRECT);
+        closCtx.closureVars = closureVars; // probably should add to caching criteria...
+        closCtx.fakeFileSource = this.fakeFileSource;
+        closCtx.clsIdeaType = this.clsIdeaType;
+        closCtx.instGetter = this.instGetter;
+        closCtx.variadicOrders = this.variadicOrders;
+        closCtx.cachedArgs = this.cachedArgs;
+        return closCtx;
     }
 
     public boolean hasArgs()

src/org/klesun/deep_assoc_completion/helpers/ExprCtx.java
@@ -71,6 +71,10 @@ public class ExprCtx implements IExprCtx {
         return subExpr(expr, funcCtx.subCtxIndirect(args, this::findExprType));
     }
 
+    public ExprCtx withClosure(L<T2<String, S<MemoizingIterable<DeepType>>>> closureVars) {
+        return subExpr(expr, funcCtx.withClosure(closureVars));
+    }
+
     public Opt<PsiElement> getFakeFileSource() {
         return funcCtx.fakeFileSource;
     }
@@ -85,6 +89,10 @@ public class ExprCtx implements IExprCtx {
 
     public Opt<PhpType> getSelfType() {
         return funcCtx.clsIdeaType;
+    }
+
+    public L<T2<String, S<MemoizingIterable<DeepType>>>> getClosureVars() {
+        return funcCtx.closureVars;
     }
 
     public It<DeepType> findExprType(PhpExpression expr) {

src/org/klesun/deep_assoc_completion/helpers/IExprCtx.java
@@ -10,14 +10,12 @@ import com.jetbrains.php.lang.psi.elements.PhpExpression;
 import com.jetbrains.php.lang.psi.elements.impl.FieldReferenceImpl;
 import com.jetbrains.php.lang.psi.resolve.types.PhpType;
 import org.klesun.deep_assoc_completion.DeepType;
-import org.klesun.lang.It;
-import org.klesun.lang.Lang;
-import org.klesun.lang.Opt;
+import org.klesun.lang.*;
 
 import java.util.Collection;
 import java.util.Map;
 
-import static org.klesun.lang.Lang.som;
+import static org.klesun.lang.Lang.*;
 
 /**
  * making it an interface since I'm refactor the context
@@ -34,8 +32,10 @@ public interface IExprCtx {
     IExprCtx subCtxDirect(NewExpression funcCall);
     IExprCtx subCtxSingleArgArr(PhpExpression argArr);
     IExprCtx subCtxIndirect(PhpExpression args);
+    IExprCtx withClosure(L<T2<String, S<MemoizingIterable<DeepType>>>> closureVars);
     It<DeepType> getThisType();
     Opt<PhpType> getSelfType();
+    L<T2<String, S<MemoizingIterable<DeepType>>>> getClosureVars();
     Opt<PsiElement> getFakeFileSource();
     Map<PsiFile, Collection<FieldReferenceImpl>> getFieldRefCache();
 

src/org/klesun/deep_assoc_completion/resolvers/ClosRes.java
@@ -85,8 +85,16 @@ public class ClosRes extends Lang
     public DeepType resolve(FunctionImpl func)
     {
         DeepType result = new DeepType(func, func.getLocalType(true));
-        result.returnTypeGetters.add((funcCtx) ->
-            new MemoizingIterable<>(getReturnedValue(func, funcCtx).iterator()));
+        result.returnTypeGetters.add((callCtx) -> {
+            L<T2<String, S<MemoizingIterable<DeepType>>>> closureVars = It(func.getChildren())
+                .fop(toCast(PhpUseList.class))
+                .fap(u -> It(u.getChildren()))
+                .fop(toCast(Variable.class))
+                .map(closVar -> T2(closVar.getName(), Tls.onDemand(() -> ctx.findExprType(closVar).mem())))
+                .arr();
+            IExprCtx closCtx = callCtx.withClosure(closureVars);
+            return new MemoizingIterable<>(getReturnedValue(func, closCtx).iterator());
+        });
         return result;
     }
 }

src/org/klesun/deep_assoc_completion/resolvers/FuncCallRes.java
@@ -414,7 +414,7 @@ public class FuncCallRes extends Lang
                 return callCtx.getArgMt(0).getInArray(call).mt().types;
             } else if (name.equals("array_intersect_key") || name.equals("array_diff_key")
                     || name.equals("array_intersect_assoc") || name.equals("array_diff_assoc")
-                    || name.equals("array_diff")
+                    || name.equals("array_diff") || name.equals("strval")
             ) {
                 // do something more clever?
                 return callCtx.getArgMt(0).types;

src/org/klesun/deep_assoc_completion/resolvers/VarRes.java
@@ -4,8 +4,7 @@ import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiWhiteSpace;
 import com.jetbrains.php.lang.documentation.phpdoc.psi.impl.PhpDocVarImpl;
 import com.jetbrains.php.lang.documentation.phpdoc.psi.tags.PhpDocTag;
-import com.jetbrains.php.lang.psi.elements.PhpExpression;
-import com.jetbrains.php.lang.psi.elements.Variable;
+import com.jetbrains.php.lang.psi.elements.*;
 import com.jetbrains.php.lang.psi.elements.impl.*;
 import com.jetbrains.php.lang.psi.resolve.types.PhpType;
 import org.klesun.deep_assoc_completion.*;
@@ -186,14 +185,22 @@ public class VarRes
     public It<DeepType> resolve(Variable variable)
     {
         It<PsiElement> references = findDeclarations(variable)
-            .flt(refPsi -> ScopeFinder.didPossiblyHappen(refPsi, variable));
+            .flt(refPsi -> ScopeFinder.didPossiblyHappen(refPsi, variable))
+            ;
 
         // @var docs are a special case since they give type
         // info from any position (above/below/right of/left of the var declaration)
         It<DeepType> docTypes = getDocType(variable);
+        Opt<Function> caretScope = Tls.findParent(variable, Function.class, a -> true)
+            .fop(func -> variable.getParent() instanceof PhpUseList
+                ? Tls.findParent(func, Function.class, a -> true) : som(func));
 
         L<Assign> asses = references
             .fop(refPsi -> {
+                Opt<Function> declScope = Tls.findParent(refPsi, Function.class, a -> true);
+                if (!declScope.equals(caretScope)) {
+                    return non(); // refPsi is outside the function, a closure, handled manually
+                }
                 boolean didSurelyHappen = ScopeFinder.didSurelyHappen(refPsi, variable);
                 return Opt.fst(() -> non()
                     , () -> (new AssRes(ctx)).collectAssignment(refPsi, didSurelyHappen)
@@ -227,9 +234,14 @@ public class VarRes
         It<DeepType> thisType = opt(variable)
             .flt(vari -> vari.getText().equals("$this"))
             .fap(vari -> ctx.getThisType());
+
+        It<DeepType> closureType = ctx.getClosureVars().itr()
+            .flt(t2 -> t2.a.equals(variable.getName()))
+            .fap(t2 -> t2.b.get());
+
         return It.cnc(
             docTypes, list(typeFromIdea),
-            thisType,
+            thisType, closureType,
             AssRes.assignmentsToTypes(asses)
         );
     }

src/org/klesun/lang/L.java
@@ -164,6 +164,11 @@ public class L<@NonNull T> extends ListWrapper<T> implements List<T>
         return itr().rdc(f, initialValue);
     }
 
+    public String toString()
+    {
+        return Tls.implode(", ", map(a -> a.toString()));
+    }
+
     /** "with" */
     public L<T> wth(Lang.C<L<T>> f) {
         f.accept(this);

src/org/klesun/lang/Opt.java
@@ -200,4 +200,9 @@ public class Opt<T> implements Iterable<T>
             })
             .has();
     }
+
+    public String toString()
+    {
+        return !has() ? "non" : "som(" + unw() + ")";
+    }
 }

commit 8fa45bea6073bcafbbb0b65cfebceda59f518921

Bugfix: incorrect closure caching caused by 2e2d72bce65395b93c9e0777c1814e1d2a0523d4

src/org/klesun/deep_assoc_completion/helpers/FuncCtx.java
@@ -195,7 +195,7 @@ public class FuncCtx extends Lang implements IFuncCtx
 
     public FuncCtx withClosure(L<T2<String, S<MemoizingIterable<DeepType>>>> closureVars)
     {
-        FuncCtx closCtx = new FuncCtx(this, argGetters, uniqueRef.def(null), EArgPsiType.INDIRECT);
+        FuncCtx closCtx = new FuncCtx(parent.def(this), argGetters, uniqueRef.def(null), EArgPsiType.INDIRECT);
         closCtx.closureVars = closureVars; // probably should add to caching criteria...
         closCtx.fakeFileSource = this.fakeFileSource;
         closCtx.clsIdeaType = this.clsIdeaType;