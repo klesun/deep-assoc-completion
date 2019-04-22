6b591d2be39934471a508b54b69a08370bbeabdb
Artur Klesun <arturklesun@gmail.com>
2019-04-22 12:17:30 +0300

    #74 Provide completion of $_SERVER keys

--- src/org/klesun/deep_assoc_completion/resolvers/VarRes.java
+++ src/org/klesun/deep_assoc_completion/resolvers/VarRes.java
@@ -10,18 +10,24 @@ import com.jetbrains.php.lang.psi.elements.*;
 import com.jetbrains.php.lang.psi.elements.impl.*;
 import com.jetbrains.php.lang.psi.resolve.types.PhpType;
 import org.klesun.deep_assoc_completion.contexts.IExprCtx;
-import org.klesun.deep_assoc_completion.helpers.*;
+import org.klesun.deep_assoc_completion.helpers.Mt;
+import org.klesun.deep_assoc_completion.helpers.ScopeFinder;
 import org.klesun.deep_assoc_completion.resolvers.var_res.ArgRes;
 import org.klesun.deep_assoc_completion.resolvers.var_res.AssRes;
 import org.klesun.deep_assoc_completion.resolvers.var_res.DocParamRes;
 import org.klesun.deep_assoc_completion.structures.Assign;
 import org.klesun.deep_assoc_completion.structures.DeepType;
 import org.klesun.deep_assoc_completion.structures.KeyType;
-import org.klesun.lang.*;
+import org.klesun.lang.It;
+import org.klesun.lang.L;
+import org.klesun.lang.Opt;
+import org.klesun.lang.Tls;
 
 import java.util.Iterator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import static org.klesun.deep_assoc_completion.structures.Mkt.*;
 import static org.klesun.lang.Lang.*;
 
 public class VarRes
@@ -238,6 +244,54 @@ public class VarRes
         );
     }
 
+    public It<DeepType> resolveSuperGlobal(Variable caretVar)
+    {
+        if (caretVar.getName().equals("_SERVER")) {
+            return assoc(caretVar, list(
+                T2("LC_PAPER", str(caretVar, "lv_LV.UTF-8").mt()),
+                T2("LC_ADDRESS", str(caretVar, "lv_LV.UTF-8").mt()),
+                T2("XDG_SESSION_ID", str(caretVar, "2766").mt()),
+                T2("LC_MONETARY", str(caretVar, "lv_LV.UTF-8").mt()),
+                T2("TERM", str(caretVar, "xterm").mt()),
+                T2("SHELL", str(caretVar, "/bin/bash").mt()),
+                T2("SSH_CLIENT", str(caretVar, "188.64.183.230 50033 22").mt()),
+                T2("LC_NUMERIC", str(caretVar, "lv_LV.UTF-8").mt()),
+                T2("SSH_TTY", str(caretVar, "/dev/pts/9").mt()),
+                T2("USER", str(caretVar, "klesun").mt()),
+                T2("LS_COLORS", str(caretVar, "rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:mi=00:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arc=01;31:*.arj=01;31:*.taz=01;31:*.lha=01;31:*.lz4=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.tzo=01;31:*.t7z=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.dz=01;31:*.gz=01;31:*.lrz=01;31:*.lz=01;31:*.lzo=01;31:*.xz=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.war=01;31:*.ear=01;31:*.sar=01;31:*.rar=01;31:*.alz=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.cab=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.webm=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.ogv=01;35:*.ogx=01;35:*.aac=00;36:*.au=00;36:*.flac=00;36:*.m4a=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;36:*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:*.oga=00;36:*.opus=00;36:*.spx=00;36:*.xspf=00;36:").mt()),
+                T2("LC_TELEPHONE", str(caretVar, "lv_LV.UTF-8").mt()),
+                T2("MAIL", str(caretVar, "/var/mail/klesun").mt()),
+                T2("PATH", str(caretVar, "/home/klesun/bin:/home/klesun/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin").mt()),
+                T2("LC_IDENTIFICATION", str(caretVar, "lv_LV.UTF-8").mt()),
+                T2("PWD", str(caretVar, "/home/klesun").mt()),
+                T2("LANG", str(caretVar, "en_US.UTF-8").mt()),
+                T2("LC_MEASUREMENT", str(caretVar, "lv_LV.UTF-8").mt()),
+                T2("SHLVL", str(caretVar, "1").mt()),
+                T2("HOME", str(caretVar, "/home/klesun").mt()),
+                T2("LOGNAME", str(caretVar, "klesun").mt()),
+                T2("XDG_DATA_DIRS", str(caretVar, "/usr/local/share:/usr/share:/var/lib/snapd/desktop").mt()),
+                T2("SSH_CONNECTION", str(caretVar, "188.64.183.230 50033 192.168.0.105 22").mt()),
+                T2("LESSOPEN", str(caretVar, "| /usr/bin/lesspipe %s").mt()),
+                T2("XDG_RUNTIME_DIR", str(caretVar, "/run/user/1000").mt()),
+                T2("LESSCLOSE", str(caretVar, "/usr/bin/lesspipe %s %s").mt()),
+                T2("LC_TIME", str(caretVar, "lv_LV.UTF-8").mt()),
+                T2("LC_NAME", str(caretVar, "lv_LV.UTF-8").mt()),
+                T2("_", str(caretVar, "/usr/bin/php").mt()),
+                T2("PHP_SELF", str(caretVar, "Standard input code").mt()),
+                T2("SCRIPT_NAME", str(caretVar, "Standard input code").mt()),
+                T2("SCRIPT_FILENAME", str(caretVar, "").mt()),
+                T2("PATH_TRANSLATED", str(caretVar, "").mt()),
+                T2("DOCUMENT_ROOT", str(caretVar, "").mt()),
+                T2("REQUEST_TIME_FLOAT", floate(caretVar, 1555923431.315248).mt()),
+                T2("REQUEST_TIME", floate(caretVar, 1555923431).mt()),
+                T2("argv", arr(caretVar, new DeepType(caretVar, PhpType.STRING).mt()).mt()),
+                T2("argc", inte(caretVar, 1).mt())
+            )).mt().types.itr();
+        } else {
+            return It.non();
+        }
+    }
+
     // add idea type only if it has class or we have no other info, since it does not
     // include string values - we may mistakenly conclude that we could not resolve type
     private static boolean hasClassInfo(PhpType ideaType)
@@ -296,8 +350,10 @@ public class VarRes
             .flt(t2 -> t2.a.equals(caretVar.getName()))
             .fap(t2 -> t2.b.get());
 
+        It<DeepType> superGlobalTit = resolveSuperGlobal(caretVar);
+
         return It.cnc(
-            docTypes,
+            docTypes, superGlobalTit,
             hasClassInfo(typeFromIdea.briefType)
                 ? list(typeFromIdea) : list(),
             thisType, closureType,
