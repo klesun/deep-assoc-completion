d28f18a9ff48d1a28f60b6044d05eb03ac6a55cd
Artur Klesun <arturklesun@gmail.com>
2020-01-28 18:32:30 +0200

    Update plugin.xml - 2020.01.28.002

---
+++ tests/someOtherFile.php
@@ -0,0 +1,8 @@
+<?php
+$reference = [ArrMethRefIssue::class, "ololo3"];
+$reference = [new ArrMethRefIssue(), "ololo3"];
+$reference = [ArrMethRefIssue::class, "ololo2"];
+$reference = [new ArrMethRefIssue(), "ololo2"];
+
+
+
